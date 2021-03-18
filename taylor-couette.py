bf6e6bc545204c7ee284a850f8631cba57ca9fb4
Jeongu Kim <jeongukim@kaist.ac.kr>
2021-03-18 11:02:43 +0900

    update python plotters

--- taylor-couette.py
+++ taylor-couette.py
@@ -7,6 +7,7 @@ Ny = 100
 L = 3.5
 ri = 0.25
 ro = 1.5
+omega = 1
 
 u = np.loadtxt("u.txt")
 v = np.loadtxt("v.txt")
@@ -24,7 +25,7 @@ yc[0] = 2*yf[0] - yc[1]
 yc[-1] = 2*yf[-1] - yc[-2]
 
 #%%
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(3, 9))
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6))
 
 ax1.set_title('u')
 x, y = np.meshgrid(xf, yc)
@@ -47,18 +48,31 @@ fig.colorbar(C3, ax=ax3)
 fig.tight_layout()
 
 #%%
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(6, 6))
 
-u = (u[1:, 1:-1] + u[:-1, 1:-1]) / 2
-v = (v[1:-1, 1:] + v[1:-1, :-1]) / 2
+ut = (u[1:, 1:-1] + u[:-1, 1:-1]) / 2
+vt = (v[1:-1, 1:] + v[1:-1, :-1]) / 2
 x, y = np.meshgrid(xc[1:-1], yc[1:-1])
 
 wi = Wedge((L/2, L/2), ri, 0, 360, color='grey', zorder=10)
 wo = Wedge((L/2, L/2), ro+1, 0, 360, width=1, color='grey', zorder=10)
 
-ax.streamplot(x, y, u.T, v.T, color='k')
+ax.streamplot(x, y, ut.T, vt.T, color='k')
 ax.add_patch(wi)
 ax.add_patch(wo)
 ax.set_aspect('equal', 'box')
+
+#%%
+fig, ax = plt.subplots()
+
+r = np.linspace(ri, ro, 21)
+ut = 1/(ro**2-ri**2) * (-omega*ri**2*r + omega*ri**2*ro**2/r)
+
+x = np.hstack((L/2-r, L/2+r))
+vt = np.hstack((ut, -ut))
+
+ax.plot(xc, v[:, Ny//2])
+ax.plot(x, vt, 'ro', markersize=3)
+ax.legend(['IBM', 'Theory'])
 
 plt.show()
